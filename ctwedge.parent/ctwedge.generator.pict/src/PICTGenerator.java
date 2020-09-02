ce6cc29e76463d103852b7d1a2361fc59574264e
Angelo Gargantini <angelo.gargantini@unibg.it>
2020-09-02 17:32:17 +0200

    ctwedge + pict

---
+++ ctwedge.parent/ctwedge.generator.pict/src/PICTGenerator.java
@@ -0,0 +1,16 @@
+import ctwedge.util.TestSuite;
+import ctwedge.util.ext.ICTWedgeTestGenerator;
+
+public class PICTGenerator extends ICTWedgeTestGenerator {
+
+	protected PICTGenerator() {
+		super("PICT");
+	}
+
+	@Override
+	public TestSuite call() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
