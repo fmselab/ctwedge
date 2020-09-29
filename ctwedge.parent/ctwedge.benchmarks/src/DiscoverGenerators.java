7f6c1b80e32a92f1bab7ea7077ab5ac5101c0a2a
Angelo Gargantini <angelo.gargantini@unibg.it>
2020-09-29 15:24:29 +0200

    discovergenerators

--- ctwedge.parent/ctwedge.benchmarks/src/DiscoverGenerators.java
+++ ctwedge.parent/ctwedge.benchmarks/src/DiscoverGenerators.java
@@ -1,25 +1,29 @@
-package xxx;
+
 import java.util.ArrayList;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtension;
 import org.eclipse.core.runtime.IExtensionPoint;
 import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.core.runtime.Platform;
+import org.junit.Test;
 
 public class DiscoverGenerators {
 
-	public static void main(String[] args) {
+	@Test
+	public void temp() throws CoreException {
 		IExtensionRegistry reg = Platform.getExtensionRegistry();
-		IExtensionPoint ep = reg.getExtensionPoint("ctwedgeGenerators");
+		IExtensionPoint ep = reg.getExtensionPoint("ctwedge.ui.ctwedgeGenerators");
 		IExtension[] extensions = ep.getExtensions();
 		ArrayList contributors = new ArrayList();
 		for (int i = 0; i < extensions.length; i++) {
 			IExtension ext = extensions[i];
+			System.out.println(ext.getExtensionPointUniqueIdentifier());
+			System.out.println(ext.getLabel());
 			IConfigurationElement[] ce = ext.getConfigurationElements();
 			for (int j = 0; j < ce.length; j++) {
-				// Object obj = ce[j].createExecutableExtension("class");
-				// contributors.add(obj);
+				//contributors.add(obj);
 				System.out.println(ce[j].toString());
 			}
 		}
