ed95968eebd6e5201d81729052973978dfce926c
a-bombarda <andrea.bombarda@unibg.it>
2020-10-09 16:59:27 +0200

    Update DiscoverGenerators.java

--- ctwedge.parent/ctwedge.benchmarks/src/DiscoverGenerators.java
+++ ctwedge.parent/ctwedge.benchmarks/src/DiscoverGenerators.java
@@ -6,6 +6,7 @@ import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtension;
 import org.eclipse.core.runtime.IExtensionPoint;
 import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.InvalidRegistryObjectException;
 import org.eclipse.core.runtime.Platform;
 import org.junit.Test;
 
@@ -26,6 +27,29 @@ public class DiscoverGenerators {
 				//contributors.add(obj);
 				System.out.println(ce[j].toString());
 			}
+		}
+	}
+	
+	@Test
+	public void temp2() throws CoreException, ClassNotFoundException, InvalidRegistryObjectException {
+		IExtensionRegistry reg = Platform.getExtensionRegistry();
+		IExtensionPoint ep = reg.getExtensionPoint("ctwedge.ui.ctwedgeGenerators");
+		IExtension[] extensions = ep.getExtensions();
+		ArrayList contributors = new ArrayList();
+		for (int i = 0; i < extensions.length; i++) {
+			IExtension ext = extensions[i];
+			IConfigurationElement[] ce = ext.getConfigurationElements();			
+			
+			for (IConfigurationElement e : ce) {
+				
+				try {
+					Class c = Class.forName(e.getAttribute("GeneratorPrototype"));
+					System.out.println(c);
+				} catch (ClassNotFoundException exc) {
+					System.err.println("Unable to find the class: " + e.getAttribute("GeneratorPrototype"));
+				}
+			}
+			
 		}
 	}
 
