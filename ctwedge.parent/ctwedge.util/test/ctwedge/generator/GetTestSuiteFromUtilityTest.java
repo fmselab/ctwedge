8799c6684cedb7f47fb3232671ae2014ebbaec85
a-bombarda <andrea.bombarda@unibg.it>
2020-10-15 08:53:50 +0200

    Update GetTestSuiteFromUtilityTest.java

--- ctwedge.parent/ctwedge.util/test/ctwedge/generator/GetTestSuiteFromUtilityTest.java
+++ ctwedge.parent/ctwedge.util/test/ctwedge/generator/GetTestSuiteFromUtilityTest.java
@@ -5,28 +5,32 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import ctwedge.generator.util.Utility;
+import ctwedge.util.ext.ICTWedgeTestGenerator;
 
 public class GetTestSuiteFromUtilityTest {
 
 	@Test
 	public void test1() throws Exception {
+		ICTWedgeTestGenerator generator = DiscoverGenerators.getGenerator("ACTSGenerator");
 		System.out.println("ACTS: " + Utility.getTestSuite(
-				"Model prova\nParameters:\n a: Boolean; b: Boolean; c: Boolean;\nConstraints:\n # a -> b #\n", DiscoverGenerators.getGenerator("ACTSGenerator"),
+				"Model prova\nParameters:\n a: Boolean; b: Boolean; c: Boolean;\nConstraints:\n # a -> b #\n", generator,
 				2, false, null));
 	}
 		
 	@Test
 	public void test2() throws Exception {
+		ICTWedgeTestGenerator generator = DiscoverGenerators.getGenerator("CASAGenerator");
 		System.out.println("CASA: " + Utility.getTestSuite(
-				"Model prova\nParameters:\n a: Boolean; b: Boolean; c: Boolean;\nConstraints:\n # a -> b #\n", DiscoverGenerators.getGenerator("CASAGenerator"),
+				"Model prova\nParameters:\n a: Boolean; b: Boolean; c: Boolean;\nConstraints:\n # a -> b #\n", generator,
 				2, false, null));
 	}
 	
 	@Test
 	public void test3() throws Exception {
-		System.out.println("PICT: " + Utility.getTestSuite(
-				"Model prova\nParameters:\n a: Boolean; b: Boolean; c: Boolean;\nConstraints:\n # a -> b #\n", DiscoverGenerators.getGenerator("PICTGenerator"),
-				2, false, null));
+		ICTWedgeTestGenerator generator = DiscoverGenerators.getGenerator("PICTGenerator");
+		System.out.println("PICT: " + generator.getTestSuite(Utility.loadModel(
+				"Model prova\nParameters:\n a: Boolean; b: Boolean; c: Boolean;\nConstraints:\n # a -> b #\n"),
+				2, false));
 	}
 	
 }
