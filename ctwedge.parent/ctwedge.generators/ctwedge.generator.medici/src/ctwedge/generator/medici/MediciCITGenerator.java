fb00c9c2fa092fbfdf1f48fe2eed5b951dded096
Angelo Gargantini <angelo.gargantini@unibg.it>
2020-10-15 10:58:37 +0200

    added medici project

---
+++ ctwedge.parent/ctwedge.generators/ctwedge.generator.medici/src/ctwedge/generator/medici/MediciCITGenerator.java
@@ -0,0 +1,21 @@
+package ctwedge.generator.medici;
+
+import ctwedge.ctWedge.CitModel;
+import ctwedge.util.TestSuite;
+import ctwedge.util.ext.ICTWedgeTestGenerator;
+
+public class MediciCITGenerator extends ICTWedgeTestGenerator{
+
+	@Override
+	public TestSuite call() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TestSuite getTestSuite(CitModel loadModel, int t, boolean ignoreC) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
