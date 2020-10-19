a006757fdeb2ce4b4c675c680081aaa7d2960a94
a-bombarda <andrea.bombarda@unibg.it>
2020-10-19 09:39:02 +0200

    Moved extension points to Util instead of Ui

--- ctwedge.parent/ctwedge.util/src/ctwedge/generator/DiscoverGenerators.java
+++ ctwedge.parent/ctwedge.util/src/ctwedge/generator/DiscoverGenerators.java
@@ -23,7 +23,7 @@ public class DiscoverGenerators {
 	// senza plugins/OSGI?
 	static public ICTWedgeTestGenerator getGenerator(String generatorName) throws CoreException, ClassNotFoundException, InvalidRegistryObjectException {
 		IExtensionRegistry reg = Platform.getExtensionRegistry();
-		IExtensionPoint ep = reg.getExtensionPoint("ctwedge.ui.ctwedgeGenerators");
+		IExtensionPoint ep = reg.getExtensionPoint("ctwedge.util.ctwedgeGenerators");
 		IExtension[] extensions = ep.getExtensions();
 		for (int i = 0; i < extensions.length; i++) {
 			IExtension ext = extensions[i];
