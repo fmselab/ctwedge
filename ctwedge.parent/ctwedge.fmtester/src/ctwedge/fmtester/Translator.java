678096ad606795f885e206ddef77edf7c088cf8b
Angelo Gargantini <angelo.gargantini@unibg.it>
2022-04-28 15:47:56 +0200

    first attempo to compute ratio

---
+++ ctwedge.parent/ctwedge.fmtester/src/ctwedge/fmtester/Translator.java
@@ -0,0 +1,19 @@
+package ctwedge.fmtester;
+
+import ctwedge.ctWedge.CitModel;
+import ctwedge.generator.util.Utility;
+import ctwedge.importer.featureide.FeatureIdeImporterMultipleLevels;
+import ctwedge.importer.featureide.XmlFeatureModelImporter;
+import ctwedge.util.ModelUtils;
+
+public class Translator {
+	
+	public static void main(String[] args) {
+		// converte da FM a ctwdege
+		XmlFeatureModelImporter importer = new XmlFeatureModelImporter();
+		CitModel model = importer.importModel("evolutionModels\\AmbientAssistedLiving\\AmbientAssistedLivingv1.xml");
+		ModelUtils utils = new ModelUtils(model);
+		System.out.println(utils.serializeToString());
+	}
+
+}
