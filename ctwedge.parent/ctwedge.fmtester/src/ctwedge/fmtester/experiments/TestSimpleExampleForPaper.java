c1a34145883f1b8f3f9b04ba4672e3031dfc9df6
Andrea Bombarda <a.bombarda@studenti.unibg.it>
2022-08-30 15:38:00 -0500

    Update TestSimpleExampleForPaper.java

--- ctwedge.parent/ctwedge.fmtester/src/ctwedge/fmtester/experiments/TestSimpleExampleForPaper.java
+++ ctwedge.parent/ctwedge.fmtester/src/ctwedge/fmtester/experiments/TestSimpleExampleForPaper.java
@@ -3,6 +3,8 @@ package ctwedge.fmtester.experiments;
 import static org.junit.Assert.*;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.List;
@@ -32,13 +34,14 @@ public class TestSimpleExampleForPaper {
 	public void test1() throws IOException, InterruptedException {
 		// distance 6 and 5
 		extracted("ex_paper1_AG", "ex_paper2_AG");
-		
+
 	}
+
 	@Test
 	public void test2() throws IOException, InterruptedException {
 		// distance 12 and 5
 		extracted("ex_paper1_AB", "ex_paper2_AB");
-		
+
 	}
 
 	public void extracted(String oldFMname, String newFMname) throws IOException, InterruptedException {
@@ -58,30 +61,128 @@ public class TestSimpleExampleForPaper {
 		// tecnica 2
 		String mediciModel = pMedici.buildMediciModel(fmName2);
 		TestModel m = Operations.readModelFromReader(new BufferedReader(new StringReader(mediciModel)));
-		ToCSV converter = new ToCSV();		
+		ToCSV converter = new ToCSV();
 		String oldTsStr = converter.toCSVcode(mediciTS1);
 		Vector<Map<String, String>> oldTs = CSVImporter.readFromReader(new StringReader(oldTsStr));
-		
+
 		String newTs = PMediciPlus.generateTests(pMedici.getModel(), m, oldTs);
 		TestSuite technique2TS = new TestSuite(newTs, pMedici.getModel());
 		float distance2 = DistancesCalculator.percTestSuitesDist(mediciTS1, technique2TS);
 		System.out.println(distance2);
 	}
-	
+
 	@Test
-	public void experimentsForPaper() {
-		
+	public void experimentsForPaper() throws IOException, InterruptedException {
+		launchSingleExperiment("ex_paper1_AG", "ex_paper2_AG");
 	}
-	
+
+	/**
+	 * Executes the single experiment
+	 * 
+	 * @param model1 : the first model
+	 * @param model2 : the evolved model
+	 * 
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	public void launchSingleExperiment(String model1, String model2) throws IOException, InterruptedException {
+		TestContext.IN_TEST = true;
+		
+		String oldModel = convertModelFromFMToCTW(model1);
+		String newModel = convertModelFromFMToCTW(model2);
+
+		// Technique 1
+		regenerationFromScratch(oldModel, newModel, 2, 1, "output.csv");
+		
+		// Technique 2
+		generateWithPMediciPlus(oldModel, newModel, 2, 1, "output.csv");
+	}
+
+	/**
+	 * Converts a FM (in xml format) into a CTW model
+	 * 
+	 * @param modelName : the name of the feature model
+	 * @return : the path of the CTW model corresponding to the feature model given
+	 *         as input
+	 * 
+	 * @throws IOException
+	 */
 	public String convertModelFromFMToCTW(String modelName) throws IOException {
-		String fmName = "fmexamples/"+ modelName;
+		String fmName = "fmexamples/" + modelName;
 		// convert to CTWedge (could be done only once)
 		Converter.fromFMtoCTWedge_ENUM(fmName + ".xml", fmName + "_ctwedge_enum.ctw");
 		return fmName + "_ctwedge_enum.ctw";
 	}
+
+	/**
+	 * Generates the test suite from scratch
+	 * 
+	 * @param oldFMname  : the path of the old FM
+	 * @param newFMname  : the path of the evolved FM
+	 * @param strength   : the strength
+	 * @param nThreads   : the number of threads to be used by pMEDICI
+	 * @param outputPath : the path of the output file where statistics are stored
+	 * 
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	public void regenerationFromScratch(String oldFMname, String newFMname, int strength, int nThreads,
+			String outputPath) throws IOException, InterruptedException {
+		PMedici pMedici = new PMedici();
+		// First model
+		TestSuite mediciTS1 = pMedici.generateTests(oldFMname, strength, nThreads);
+		// Second model
+		TestSuite mediciTS2 = pMedici.generateTests(newFMname, strength, nThreads);
+
+		// Distance
+		float distance = DistancesCalculator.testSuitesDist(mediciTS1, mediciTS2);
+
+		// Write statistics to file
+		FileWriter fw = new FileWriter(outputPath, true);
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.write("T1;" + oldFMname + ";" + mediciTS1.getTests().size() + ";" + mediciTS1.getGeneratorTime() + ";"
+				+ newFMname + ";" + mediciTS2.getTests().size() + ";" + mediciTS2.getGeneratorTime() + ";" + distance
+				+ ";");
+		bw.newLine();
+		bw.close();
+	}
 	
-	public void regenerationFromScratch(String oldFMname, String newFMname) {
-		
-		
+	/**
+	 * Generates the test suite with pMEDICI+
+	 * 
+	 * @param oldFMname  : the path of the old FM
+	 * @param newFMname  : the path of the evolved FM
+	 * @param strength   : the strength
+	 * @param nThreads   : the number of threads to be used by pMEDICI
+	 * @param outputPath : the path of the output file where statistics are stored
+	 * 
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	public void generateWithPMediciPlus(String oldFMname, String newFMname, int strength, int nThreads,
+			String outputPath) throws IOException, InterruptedException {
+		PMedici pMedici = new PMedici();
+		// First model
+		TestSuite mediciTS1 = pMedici.generateTests(oldFMname, strength, nThreads);
+		// Second model
+		String mediciModel = pMedici.buildMediciModel(newFMname);
+		TestModel m = Operations.readModelFromReader(new BufferedReader(new StringReader(mediciModel)));
+		ToCSV converter = new ToCSV();
+		String oldTsStr = converter.toCSVcode(mediciTS1);
+		Vector<Map<String, String>> oldTs = CSVImporter.readFromReader(new StringReader(oldTsStr));
+		String newTs = PMediciPlus.generateTests(pMedici.getModel(), m, oldTs);
+		TestSuite mediciTS2 = new TestSuite(newTs, pMedici.getModel());
+
+		// Distance
+		float distance = DistancesCalculator.testSuitesDist(mediciTS1, mediciTS2);
+
+		// Write statistics to file
+		FileWriter fw = new FileWriter(outputPath, true);
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.write("T2;" + oldFMname + ";" + mediciTS1.getTests().size() + ";" + mediciTS1.getGeneratorTime() + ";"
+				+ newFMname + ";" + mediciTS2.getTests().size() + ";" + mediciTS2.getGeneratorTime() + ";" + distance
+				+ ";");
+		bw.newLine();
+		bw.close();
 	}
 }
