89a95bfd4c01f4d7ab9662dff542f27ebcd2ac38
a-bombarda <andrea.bombarda@unibg.it>
2022-09-14 15:15:30 +0200

    Update

--- ctwedge.parent/ctwedge.fmtester/src/ctwedge/fmtester/experiments/TestSimpleExampleForPaper.java
+++ ctwedge.parent/ctwedge.fmtester/src/ctwedge/fmtester/experiments/TestSimpleExampleForPaper.java
@@ -27,6 +27,7 @@ import ctwedge.generator.exporter.ToCSV;
 import ctwedge.importer.featureide.FeatureIdeImporter;
 import ctwedge.importer.featureide.XmlFeatureModelImporter;
 import ctwedge.util.TestSuite;
+import ctwedge.util.validator.MinimalityTestSuiteValidator;
 import de.ovgu.featureide.fm.core.ExtensionManager.NoSuchExtensionException;
 import de.ovgu.featureide.fm.core.analysis.cnf.formula.FeatureModelFormula;
 import de.ovgu.featureide.fm.core.base.IFeatureModel;
@@ -52,6 +53,8 @@ import de.ovgu.featureide.fm.core.configuration.ConfigurationPropagator;
 
 public class TestSimpleExampleForPaper {
 
+	static boolean REDUCE_TEST_SUITE = true;
+	
 	@Test
 	public void test1() throws IOException, InterruptedException {
 		// distance 6 and 5
@@ -98,25 +101,30 @@ public class TestSimpleExampleForPaper {
 			throws IOException, InterruptedException, UnsupportedModelException, NoSuchExtensionException {		
 		TestBuilder.KeepPartialOldTests = true;
 		int N_REPETITIONS = 10;
+		int[] nThreadsList = new int[] {1, 2, 4, 6, 8};
 		
 		for (int i=0; i<N_REPETITIONS; i++) {
-			// Example in paper
-			// launchSingleExperiment("ex_paper1_AG", "ex_paper2_AG", "fmexamples/");
-			
-			launchMultipleExperiment(new String[] {"PPUv1", "PPUv2", "PPUv3", "PPUv4", "PPUv5", "PPUv6", "PPUv7", "PPUv8", "PPUv9"}, "evolutionModels/PPU/");
-			launchMultipleExperiment(new String[] {"AmbientAssistedLivingv1", "AmbientAssistedLivingv2"}, "evolutionModels/AmbientAssistedLiving/");
-			launchMultipleExperiment(new String[] {"AutomotiveMultimediav1", "AutomotiveMultimediav2", "AutomotiveMultimediav3"}, "evolutionModels/AutomotiveMultimedia/");
-			launchMultipleExperiment(new String[] {"Boeingv1", "Boeingv2", "Boeingv3"}, "evolutionModels/Boeing/");
-			launchMultipleExperiment(new String[] {"CarBodyv1", "CarBodyv2", "CarBodyv3", "CarBodyv4"}, "evolutionModels/CarBody/");
-			launchMultipleExperiment(new String[] {"LinuxKernelv1", "LinuxKernelv2", "LinuxKernelv3"}, "evolutionModels/LinuxKernel/");
-			launchMultipleExperiment(new String[] {"ParkingAssistantv1", "ParkingAssistantv2", "ParkingAssistantv3", "ParkingAssistantv4", "ParkingAssistantv5"}, "evolutionModels/ParkingAssistant/");
-			launchMultipleExperiment(new String[] {"SmartHotelv1", "SmartHotelv2"}, "evolutionModels/SmartHotel/");
-			launchMultipleExperiment(new String[] {"SmartWatchv1", "SmartWatchv2"}, "evolutionModels/SmartWatch/");
-			launchMultipleExperiment(new String[] {"WeatherStationv1", "WeatherStationv2"}, "evolutionModels/WeatherStation/");
-			launchMultipleExperiment(new String[] {"ERP_SPL_s1", "ERP_SPL_s2"}, "evolutionModels/ERP/");
-			launchMultipleExperiment(new String[] {"HelpSystem1", "HelpSystem2"}, "evolutionModels/HelpSystem/");
-			launchMultipleExperiment(new String[] {"MobileMediaV3", "MobileMediaV4", "MobileMediaV5", "MobileMediaV6", "MobileMediaV7", "MobileMediaV8"}, "evolutionModels/MobileMedia/");
-			launchMultipleExperiment(new String[] {"SmartHomeV2", "SmartHomeV2.2"}, "evolutionModels/SmartHome/");
+			for (int nThreads : nThreadsList) {
+				
+				// Example in paper
+				// launchSingleExperiment("ex_paper1_AG", "ex_paper2_AG", "fmexamples/");
+				
+				launchMultipleExperiment(new String[] {"PPUv1", "PPUv2", "PPUv3", "PPUv4", "PPUv5", "PPUv6", "PPUv7", "PPUv8", "PPUv9"}, "evolutionModels/PPU/", nThreads);
+				launchMultipleExperiment(new String[] {"AmbientAssistedLivingv1", "AmbientAssistedLivingv2"}, "evolutionModels/AmbientAssistedLiving/", nThreads);
+				launchMultipleExperiment(new String[] {"AutomotiveMultimediav1", "AutomotiveMultimediav2", "AutomotiveMultimediav3"}, "evolutionModels/AutomotiveMultimedia/", nThreads);
+				launchMultipleExperiment(new String[] {"Boeingv1", "Boeingv2", "Boeingv3"}, "evolutionModels/Boeing/", nThreads);
+				launchMultipleExperiment(new String[] {"CarBodyv1", "CarBodyv2", "CarBodyv3", "CarBodyv4"}, "evolutionModels/CarBody/", nThreads);
+				launchMultipleExperiment(new String[] {"LinuxKernelv1", "LinuxKernelv2", "LinuxKernelv3"}, "evolutionModels/LinuxKernel/", nThreads);
+				launchMultipleExperiment(new String[] {"ParkingAssistantv1", "ParkingAssistantv2", "ParkingAssistantv3", "ParkingAssistantv4", "ParkingAssistantv5"}, "evolutionModels/ParkingAssistant/", nThreads);
+				launchMultipleExperiment(new String[] {"SmartHotelv1", "SmartHotelv2"}, "evolutionModels/SmartHotel/", nThreads);
+				launchMultipleExperiment(new String[] {"SmartWatchv1", "SmartWatchv2"}, "evolutionModels/SmartWatch/", nThreads);
+				launchMultipleExperiment(new String[] {"WeatherStationv1", "WeatherStationv2"}, "evolutionModels/WeatherStation/", nThreads);
+				launchMultipleExperiment(new String[] {"ERP_SPL_s1", "ERP_SPL_s2"}, "evolutionModels/ERP/", nThreads);
+				launchMultipleExperiment(new String[] {"HelpSystem1", "HelpSystem2"}, "evolutionModels/HelpSystem/", nThreads);
+				launchMultipleExperiment(new String[] {"MobileMediaV3", "MobileMediaV4", "MobileMediaV5", "MobileMediaV6", "MobileMediaV7", "MobileMediaV8"}, "evolutionModels/MobileMedia/", nThreads);
+				launchMultipleExperiment(new String[] {"SmartHomeV2", "SmartHomeV2.2"}, "evolutionModels/SmartHome/", nThreads);
+				// launchMultipleExperiment(new String[] {"automotive2_1", "automotive2_2", "automotive2_3", "automotive2_4"}, "evolutionModels/Automotive/");
+			}
 		}
 	}
 	
@@ -125,17 +133,18 @@ public class TestSimpleExampleForPaper {
 	 * 
 	 * @param models : the list of models
 	 * @param path   : the path in which models are stored
+	 * @param nThreads : the number of threads to be used
 	 * 
 	 * @throws IOException
 	 * @throws InterruptedException
 	 * @throws NoSuchExtensionException
 	 * @throws UnsupportedModelException
 	 */
-	public void launchMultipleExperiment(String[] models, String path) throws IOException, InterruptedException, UnsupportedModelException, NoSuchExtensionException {
+	public void launchMultipleExperiment(String[] models, String path, int nThreads) throws IOException, InterruptedException, UnsupportedModelException, NoSuchExtensionException {
 		for (int i=0; i<models.length-1; i++) {
 			int j = i+1;
 			//for (int j=i+1; j<models.length; j++) {
-				launchSingleExperiment(models[i], models[j], path);
+				launchSingleExperiment(models[i], models[j], path, nThreads);
 			//}
 		}
 	}
@@ -146,13 +155,14 @@ public class TestSimpleExampleForPaper {
 	 * @param model1 : the first model
 	 * @param model2 : the evolved model
 	 * @param path   : the path in which models are stored
+	 * @param nThreads : the number of threads to be used
 	 * 
 	 * @throws IOException
 	 * @throws InterruptedException
 	 * @throws NoSuchExtensionException
 	 * @throws UnsupportedModelException
 	 */
-	public void launchSingleExperiment(String model1, String model2, String path)
+	public void launchSingleExperiment(String model1, String model2, String path, int nThreads)
 			throws IOException, InterruptedException, UnsupportedModelException, NoSuchExtensionException {
 		TestContext.IN_TEST = true;
 
@@ -160,10 +170,10 @@ public class TestSimpleExampleForPaper {
 		String newModel = convertModelFromFMToCTW(model2, path);
 
 		// Technique 1
-		TestSuite oldTs = regenerationFromScratch(oldModel, newModel, 2, 1, "output.csv");
+		TestSuite oldTs = regenerationFromScratch(oldModel, newModel, 2, nThreads, "output.csv");
 
 		// Technique 2
-		generateWithPMediciPlus(oldModel, newModel, oldTs, 2, 1, "output.csv");
+		generateWithPMediciPlus(oldModel, newModel, oldTs, 2, nThreads, "output.csv");
 	}
 
 	/**
@@ -207,7 +217,13 @@ public class TestSimpleExampleForPaper {
 		TestSuite mediciTS1 = pMedici.generateTests(oldFMname, strength, nThreads);
 		// Second model
 		TestSuite mediciTS2 = pMedici.generateTests(newFMname, strength, nThreads);
-
+		
+		// Minimize test suites
+		if (REDUCE_TEST_SUITE) {
+			mediciTS1 = reduceTestSuite(mediciTS1);
+			mediciTS2 = reduceTestSuite(mediciTS2);
+		}
+		
 		// Distance
 		float distance = DistancesCalculator.testSuitesDist(mediciTS1, mediciTS2);
 		// Mutation score
@@ -218,11 +234,25 @@ public class TestSimpleExampleForPaper {
 		BufferedWriter bw = new BufferedWriter(fw);
 		bw.write("T1;" + oldFMname + ";" + mediciTS1.getTests().size() + ";" + mediciTS1.getGeneratorTime() + ";"
 				+ newFMname + ";" + mediciTS2.getTests().size() + ";" + mediciTS2.getGeneratorTime() + ";" + distance
-				+ ";" + faultDetectionCapability + ";");
+				+ ";" + faultDetectionCapability + ";" + nThreads + ";");
 		bw.newLine();
 		bw.close();
 
 		return mediciTS1;
+	}
+
+	/**
+	 * Reduces the test suite given as parameter
+	 * 
+	 * @param ts : the test suite to be reduced
+	 * @return the reduced test suite
+	 */
+	private TestSuite reduceTestSuite(TestSuite ts) {
+		MinimalityTestSuiteValidator minimality = new MinimalityTestSuiteValidator(ts);
+		long start = System.currentTimeMillis();
+		TestSuite tsReduced = minimality.reduceSize();
+		tsReduced.setGeneratorTime(System.currentTimeMillis() - start + ts.getGeneratorTime());
+		return tsReduced;
 	}
 
 	/**
@@ -247,6 +277,7 @@ public class TestSimpleExampleForPaper {
 		// First model
 		TestSuite mediciTS1 = originalTS;
 		Collections.shuffle(mediciTS1.getTests());
+		
 		// Second model
 		String mediciModel = pMedici.buildMediciModel(newFMname);
 		TestModel m = Operations.readModelFromReader(new BufferedReader(new StringReader(mediciModel)));
@@ -257,18 +288,34 @@ public class TestSimpleExampleForPaper {
 		String newTs = PMediciPlus.generateTests(pMedici.getModel(), m, oldTs);
 		TestSuite mediciTS2 = new TestSuite(newTs, pMedici.getModel());
 		mediciTS2.setGeneratorTime(System.currentTimeMillis() - start);
-
+		mediciTS2.setStrength(nThreads);
+		
 		// Distance
 		float distance = DistancesCalculator.testSuitesDist(mediciTS1, mediciTS2);
 		// Mutation score
 		float faultDetectionCapability = computeFaultDetectionCapability(newFMname, mediciTS2);
-
-		// Write statistics to file
+		
+		// Write statistics to file before minimization
 		FileWriter fw = new FileWriter(outputPath, true);
 		BufferedWriter bw = new BufferedWriter(fw);
 		bw.write("T2;" + oldFMname + ";" + mediciTS1.getTests().size() + ";" + mediciTS1.getGeneratorTime() + ";"
 				+ newFMname + ";" + mediciTS2.getTests().size() + ";" + mediciTS2.getGeneratorTime() + ";" + distance
-				+ ";" + faultDetectionCapability + ";");
+				+ ";" + faultDetectionCapability + ";" + nThreads + ";");
+		bw.newLine();
+		
+		// Minimize test suite
+		if (REDUCE_TEST_SUITE)
+			mediciTS2 = reduceTestSuite(mediciTS2);
+
+		// Distance
+		distance = DistancesCalculator.testSuitesDist(mediciTS1, mediciTS2);
+		// Mutation score
+		faultDetectionCapability = computeFaultDetectionCapability(newFMname, mediciTS2);
+
+		// Write statistics to file
+		bw.write("T2Reducted;" + oldFMname + ";" + mediciTS1.getTests().size() + ";" + mediciTS1.getGeneratorTime() + ";"
+				+ newFMname + ";" + mediciTS2.getTests().size() + ";" + mediciTS2.getGeneratorTime() + ";" + distance
+				+ ";" + faultDetectionCapability + ";" + nThreads + ";");
 		bw.newLine();
 		bw.close();
 	}
