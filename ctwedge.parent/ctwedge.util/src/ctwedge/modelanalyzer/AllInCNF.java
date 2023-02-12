82091f6a597b7f82f11b80cc503078431b5d6881
a-bombarda <andrea.bombarda@unibg.it>
2023-02-12 15:22:32 +0100

    Update data

--- ctwedge.parent/ctwedge.util/src/ctwedge/modelanalyzer/AllInCNF.java
+++ ctwedge.parent/ctwedge.util/src/ctwedge/modelanalyzer/AllInCNF.java
@@ -9,6 +9,8 @@ import ctwedge.ctWedge.AtomicPredicate;
 import ctwedge.ctWedge.CitModel;
 import ctwedge.ctWedge.Constraint;
 import ctwedge.ctWedge.Expression;
+import ctwedge.ctWedge.NotExpression;
+import ctwedge.ctWedge.Operators;
 import ctwedge.ctWedge.OrExpression;
 
 // constraints are forbidden tuples
@@ -29,15 +31,11 @@ public class AllInCNF extends CTWedgeModelAnalyzer{
 			List<Expression> splitAnd = splitAnd(e);		
 			return splitAnd.stream().allMatch(x -> notContainsAnd(x));
 		}
-		/*else if (c instanceof NotExpression) {
-			// !((...) OR (....))
-			List<Expression> splitor = splitOr(((NotExpression) c).getPredicate());
-			
-			
-			
-			
-			return splitor.stream().allMatch(x -> isSimpleEqual(x, Operators.NE));
-		}*/
+//		else if (c instanceof NotExpression) {
+//			// !((...) OR (....))
+//			List<Expression> splitor = splitOr(((NotExpression) c).getPredicate());
+//			return splitor.stream().allMatch(x -> isCNF((Expression) x));
+//		}
 		return false;
 		
 	}
