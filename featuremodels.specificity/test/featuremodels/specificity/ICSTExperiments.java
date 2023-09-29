167be8e86402727887ba8f93fbdcbf79fcb3e8c3
a-bombarda <andrea.bombarda@unibg.it>
2023-09-29 10:38:17 +0200

    Add fault detection computation

--- featuremodels.specificity/test/featuremodels/specificity/ICSTExperiments.java
+++ featuremodels.specificity/test/featuremodels/specificity/ICSTExperiments.java
@@ -2,52 +2,71 @@ package featuremodels.specificity;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Iterator;
+import java.util.List;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import ctwedge.ctWedge.CitModel;
+import ctwedge.fmtester.experiments.MutationScore;
 import ctwedge.fmtester.experiments.TestSimpleExampleForPaper;
+import ctwedge.fmtester.experiments.MutationScore.MissingFeatureTreatment;
 import ctwedge.generator.acts.ACTSTranslator;
 import ctwedge.importer.featureide.FeatureIdeImporter;
 import ctwedge.importer.featureide.FeatureIdeImporterBoolean;
 import ctwedge.util.TestSuite;
 import ctwedge.util.validator.MinimalityTestSuiteValidator;
+import de.ovgu.featureide.fm.core.ExtensionManager.NoSuchExtensionException;
 import de.ovgu.featureide.fm.core.base.IFeatureModel;
 import de.ovgu.featureide.fm.core.init.FMCoreLibrary;
+import de.ovgu.featureide.fm.core.io.UnsupportedModelException;
 import de.ovgu.featureide.fm.core.io.manager.FeatureModelManager;
+import fmautorepair.mutationoperators.FMMutation;
+import fmautorepair.mutationoperators.FMMutator;
+import fmautorepair.mutationprocess.FMMutationProcess;
 
 public class ICSTExperiments {
+
+	public static int N_REP = 10;
 
 	static {
 		FMCoreLibrary.getInstance().install();
 	}
 
 	@Test
-	public void testExperiments() throws IOException {
-		Logger.getLogger(BDDCITTestGenerator.class).setLevel(Level.OFF);
-		testEvo(TestSimpleExampleForPaper.EV_ALIV);
-		testEvo(TestSimpleExampleForPaper.EV_PPU);
-		testEvo(TestSimpleExampleForPaper.EV_AUTOM);
-		testEvo(TestSimpleExampleForPaper.EV_BOING);
-		testEvo(TestSimpleExampleForPaper.EV_CARBODY);
-		testEvo(TestSimpleExampleForPaper.EV_LINUX);
-		testEvo(TestSimpleExampleForPaper.EV_PARKING);
-		testEvo(TestSimpleExampleForPaper.EV_BCS);
-		testEvo(TestSimpleExampleForPaper.EV_ERP);
-		testEvo(TestSimpleExampleForPaper.EV_HSYS);
-		testEvo(TestSimpleExampleForPaper.EV_MOBMEDIA);
-		testEvo(TestSimpleExampleForPaper.EV_SHOME);
-		testEvo(TestSimpleExampleForPaper.EV_SMARTH);
-		testEvo(TestSimpleExampleForPaper.EV_SMARTW);
-		testEvo(TestSimpleExampleForPaper.EV_WSTAT);
+	public void testExperiments() throws IOException, UnsupportedModelException, NoSuchExtensionException {
+		String output_file = "resultsSPECIFICITY.csv";
+		BufferedWriter fw = new BufferedWriter(new FileWriter(new File(output_file), false));
+		fw.write("OldFM;NewFM;Generator;Size;Time;NSpecificTests;NNotSpecificTests;Specificity;FaultDetection\n");
+		fw.close();
+
+		for (int i = 0; i < N_REP; i++) {
+			Logger.getLogger(BDDCITTestGenerator.class).setLevel(Level.OFF);
+			testEvo(TestSimpleExampleForPaper.EV_ALIV);
+			testEvo(TestSimpleExampleForPaper.EV_PPU);
+			testEvo(TestSimpleExampleForPaper.EV_AUTOM);
+			testEvo(TestSimpleExampleForPaper.EV_BOING);
+			testEvo(TestSimpleExampleForPaper.EV_CARBODY);
+			testEvo(TestSimpleExampleForPaper.EV_LINUX);
+			testEvo(TestSimpleExampleForPaper.EV_PARKING);
+			testEvo(TestSimpleExampleForPaper.EV_BCS);
+			testEvo(TestSimpleExampleForPaper.EV_ERP);
+			testEvo(TestSimpleExampleForPaper.EV_HSYS);
+			testEvo(TestSimpleExampleForPaper.EV_MOBMEDIA);
+			testEvo(TestSimpleExampleForPaper.EV_SHOME);
+			testEvo(TestSimpleExampleForPaper.EV_SMARTH);
+			testEvo(TestSimpleExampleForPaper.EV_SMARTW);
+			testEvo(TestSimpleExampleForPaper.EV_WSTAT);
+		}
 	}
 
-	private void testEvo(String[] evo) throws IOException {
+	private void testEvo(String[] evo) throws IOException, UnsupportedModelException, NoSuchExtensionException {
 		for (int i = 1; i < evo.length - 1; i++) {
 			for (int j = 1; j < evo.length - 1; j++)
 				if (i != j)
@@ -56,7 +75,8 @@ public class ICSTExperiments {
 		}
 	}
 
-	private void executeTest(String oldFm, String newFm) throws IOException {
+	private void executeTest(String oldFm, String newFm)
+			throws IOException, UnsupportedModelException, NoSuchExtensionException {
 		// Output files
 		String output_file = "resultsSPECIFICITY.csv";
 		BufferedWriter fw = new BufferedWriter(new FileWriter(new File(output_file), true));
@@ -88,7 +108,8 @@ public class ICSTExperiments {
 		// SPECIFICITY output
 		fw.write(new File(oldFm).getName() + ";" + new File(newFm).getName() + ";" + tsReduced.getGeneratorName() + ";"
 				+ tsReduced.getTests().size() + ";" + tsReduced.getGeneratorTime() + ";" + countSpec + ";"
-				+ countNotSpec + ";" + (float) countSpec / tsReduced.getTests().size() + "\n");
+				+ countNotSpec + ";" + (float) countSpec / tsReduced.getTests().size() + ";"
+				+ computeFaultDetectionCapability(newFMPath, tsReduced) + "\n");
 
 		// Generate with ACTS
 		ACTSTranslator acts = new ACTSTranslator();
@@ -106,9 +127,77 @@ public class ICSTExperiments {
 		// ACTS output
 		fw.write(new File(oldFm).getName() + ";" + new File(newFm).getName() + ";" + tsACTS.getGeneratorName() + ";"
 				+ tsACTS.getTests().size() + ";" + tsACTS.getGeneratorTime() + ";" + countSpec + ";" + countNotSpec
-				+ ";" + (float) countSpec / tsACTS.getTests().size() + "\n");
+				+ ";" + (float) countSpec / tsACTS.getTests().size() + ";"
+				+ computeFaultDetectionCapability(newFMPath, tsACTS) + "\n");
+
+		// Generate with BDD without specificity
+		BDDCITTestGenerator genBDD = new BDDCITTestGenerator(newFM, 2);
+		ts = genBDD.generateTestSuite();
+		ts.setStrength(2);
+		minimality = new MinimalityTestSuiteValidator(ts);
+		tsReduced = minimality.reduceSize();
+		spcheck = new SpecificityChecker(oldFM, newFM, false);
+		countSpec = 0;
+		countNotSpec = 0;
+		for (ctwedge.util.Test t : tsReduced.getTests()) {
+			if (spcheck.isSpecific(t))
+				countSpec++;
+			else
+				countNotSpec++;
+		}
+
+		// BDD output
+		fw.write(new File(oldFm).getName() + ";" + new File(newFm).getName() + ";" + tsReduced.getGeneratorName() + ";"
+				+ tsReduced.getTests().size() + ";" + tsReduced.getGeneratorTime() + ";" + countSpec + ";"
+				+ countNotSpec + ";" + (float) countSpec / tsReduced.getTests().size() + ";"
+				+ computeFaultDetectionCapability(newFMPath, tsReduced) + "\n");
 
 		fw.close();
+	}
+
+	/**
+	 * Computes the fault detection capability of the evolved test suite
+	 * 
+	 * @param fm : the new feature model path
+	 * @param ts : the new test suite
+	 * @return : the fault detection capability of the evolved test suite
+	 * @throws NoSuchExtensionException
+	 * @throws UnsupportedModelException
+	 * @throws FileNotFoundException
+	 */
+	private float computeFaultDetectionCapability(Path fm, TestSuite ts)
+			throws FileNotFoundException, UnsupportedModelException, NoSuchExtensionException {
+		float totMut = 0;
+		float killedMut = 0;
+
+		IFeatureModel fm2 = FeatureModelManager.load(fm);
+
+		// Define the mutators
+		@SuppressWarnings("deprecation")
+		List<FMMutator> mutatorList = FMMutationProcess.allMutationOperatorsAsList();
+
+		// Apply the mutations
+		for (FMMutator mut : mutatorList) {
+			// Fetch all the obtained mutants
+			Iterator<FMMutation> mutations = mut.mutate(fm2);
+			while (mutations.hasNext()) {
+				totMut++;
+				FMMutation fmM = mutations.next();
+
+				// Transform the test to a configuration
+				IFeatureModel featureModel = fmM.getFirst();
+				for (ctwedge.util.Test test : ts.getTests()) {
+					MutationScore.treat_missing_feature_as = MissingFeatureTreatment.SKIP;
+					Boolean result = MutationScore.isTestValidBool(featureModel, test);
+					if (!result) {
+						killedMut++;
+						break;
+					}
+				}
+			}
+		}
+
+		return totMut != 0 ? killedMut / totMut : 0;
 	}
 
 }
