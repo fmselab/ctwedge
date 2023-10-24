1b9c8a05f409bdd11e6f9e678192ac23ed403aec
a-bombarda <andrea.bombarda@unibg.it>
2023-10-24 14:34:06 +0200

    Update dependencies

--- ctwedge.parent/ctwedge.importers/ctwedge.importer.featureide.tests/src/ctwedge/importer/featureide/ConfigurationBuilder.java
+++ ctwedge.parent/ctwedge.importers/ctwedge.importer.featureide.tests/src/ctwedge/importer/featureide/ConfigurationBuilder.java
@@ -2,8 +2,11 @@ package ctwedge.importer.featureide;
 
 import java.nio.file.Path;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
+//import ctwedge.fmtester.experiments.MutationScore;
 import de.ovgu.featureide.fm.core.analysis.cnf.formula.FeatureModelFormula;
 import de.ovgu.featureide.fm.core.base.IFeatureModel;
 import de.ovgu.featureide.fm.core.configuration.Configuration;
@@ -15,17 +18,20 @@ import de.ovgu.featureide.fm.core.io.manager.FeatureModelManager;
 
 public class ConfigurationBuilder {
 
+	static {
+		FMCoreLibrary.getInstance().install();
+	}
+	
 	@Test
 	public void test() {
-		FMCoreLibrary.getInstance().install();
 //		FMFactoryManager.getInstance().addExtension(DefaultFeatureModelFactory.getInstance());
 //		FMFormatManager.getInstance().addExtension(new XmlFeatureModelFormat());
-		Path path = Path.of("models/featureide/aircraft_fm.xml");
-		IFeatureModel featureModel = FeatureModelManager.load(path );
-		Configuration configuration = new Configuration(new FeatureModelFormula(featureModel));
-		configuration.setManual("Engine", Selection.SELECTED);
-		System.out.println(configuration.getUnselectedFeatureNames());
-		System.out.println(configuration.getUndefinedFeatureNames());
+//		Path path = Path.of("models/featureide/aircraft_fm.xml");
+//		IFeatureModel featureModel = FeatureModelManager.load(path );
+//		Configuration configuration = new Configuration(new FeatureModelFormula(featureModel));
+//		configuration.setManual("Engine", Selection.SELECTED);
+//		System.out.println(configuration.getUnselectedFeatureNames());
+//		System.out.println(configuration.getUndefinedFeatureNames());
 		
 	}
 
